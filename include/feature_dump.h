e795e5802d22d97c66d3d5956999cce61e5d3a48
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-17 14:13:05 +0330

    Create feature_dump.h

---
+++ include/feature_dump.h
@@ -0,0 +1,7 @@
+#ifndef FEATURE_DUMP_H
+#define FEATURE_DUMP_H
+
+/* Small helper to write a JSON-style feature dump */
+void write_feature_dump(const char *out_path);
+
+#endif /* FEATURE_DUMP_H */
